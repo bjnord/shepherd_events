7d60f4c61eaf5476158b552cbd0e7c1573939ed9
Brent J. Nordquist <brent@nordist.net>
2013-11-23 13:27:56 -0600

    Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,30 @@
+# Capistrano settings
 require 'bundler/capistrano'
-
 require 'capistrano/ext/multistage'
-set :stages, %w(preproduction)
-set :default_stage, 'preproduction'
+set :stages, %w(production)
+set :default_stage, 'production'
 
 # application settings
-# set :application, 'your_application_name_here'
+set :application, 'shepherd_events'
 
 # SCM settings
-# set :repository, 'your_repository_path_here'
+set :repository, 'https://github.com/bjnord/shepherd_events.git'
 set :scm, :git
 set :scm_verbose, true
 set :deploy_via, :remote_cache
 
 # other settings
 set :use_sudo, false
-set :ssh_options, { forward_agent: true }
+#set :ssh_options, { forward_agent: true }
 set :keep_releases, 12
+after 'deploy:update_code', 'deploy:symlink_app_config'
 after 'deploy', 'deploy:cleanup'
 after 'deploy:migrations', 'deploy:cleanup'
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# custom tasks
+namespace :deploy do
+  desc "Symlinks the app_config.yml"
+  task :symlink_app_config, :roles => [:web, :app, :db] do
+    run "ln -nfs #{deploy_to}/shared/config/app_config.yml #{release_path}/config/app_config.yml"
+  end
+end
--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -1,7 +1,15 @@
 # preproduction stage
 
-# set :user, 'username'
-# server 'server.example.com', :app, :web, :db, :primary => true
-# set :deploy_to, '/path/on/server'
+set :user, 'ec2-user'
+server 'yourserverhere.compute.amazonaws.com', :app, :web, :db, :primary => true
+set :deploy_to, '/home/ec2-user/shepherd_events'
 
 set :rails_env, 'production'
+
+# Unicorn settings
+require 'capistrano-unicorn'
+set :unicorn_config_filename, 'unicorn/production.rb'
+#after 'deploy:restart', 'unicorn:duplicate' # preload_app + before_fork
+# FIXME unicorn:duplicate doesn't seem to pick up our changes
+# <http://stackoverflow.com/questions/9388074/restarting-unicorn-with-usr2-doesnt-seem-to-reload-production-rb-settings>
+after 'deploy:restart', 'unicorn:restart' # app preloaded
