824db459dc0537e2cba4412fdaf3d38685865c58
Brent J. Nordquist <brent@nordist.net>
2013-12-18 07:23:43 -0600

    Final fix for cron bug (wrapper script)

--- bin/fetch-events.sh
+++ bin/fetch-events.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 if [ -e $HOME/shepherd_events/current ]; then
     DIR=$HOME/shepherd_events/current
 else
     DIR=$HOME/shepherd_events
 fi
+RAILS_ENV=production
+export RAILS_ENV DIR
 cd $DIR || exit
-bundle exec rails runner bin/fetch-events -V
+source /usr/local/rvm/environments/ruby-2.0.0-p353
+rails runner bin/fetch-events -V
